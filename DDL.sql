540465e64c40385e47a90278f7bd1d0a337b0fbf
paulohenrique7010 <paulohenrique7010@gmail.com>
2020-09-16 09:29:01 -0300

    Alterado codigo sql

--- DDL.sql
+++ DDL.sql
@@ -1,15 +1,31 @@
-create schema anotacao; 
 
-use anotacao;
- 
-create user 'user'@'localhost' identified by 'pass123'; 
+create database spring;
 
-grant select, insert, delete, update on anotacao.* to user@'localhost';
- 
-create table usr usuario ( 
-  usr_id bigint unsigned not null auto increment, 
-  usr_nome varchar(20) not null, 
-  usr senha varchar(50) not null, 
-  primary key (usr_id), 
-  unique key uni_usuarfo_nome (usr_nome) 
+use spring;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on spring.* to user@'localhost';
+
+create table usuario (
+	id_usuario bigint unsigned not null auto_increment,
+	nome varchar(20) not null,
+	senha varchar(50) not null,
+	primary key (id_usuario),
+	unique key uni_nome (nome)
+);
+
+create table autorizacao(
+	id_aut bigint unsigned not null auto_increment,
+	aut_nome varchar(20) not null,
+	primary key (id_aut),
+	unique key uni_aut_nome (aut_nome)
+);
+
+create table usuario_autorizacao(
+	id_usuario bigint unsigned not null,
+	id_aut bigint unsigned not null,
+	primary key (id_usuario, id_aut),
+	foreign key usuario_fk (id_usuario) references usuario (id_usuario),
+	foreign key aut_fk (id_aut) references autorizacao (id_aut)
 );
