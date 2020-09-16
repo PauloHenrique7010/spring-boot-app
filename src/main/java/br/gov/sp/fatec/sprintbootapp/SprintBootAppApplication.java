0b5ea5a11064d85bb94cb9429b63edacaff10690
paulohenrique7010 <paulohenrique7010@gmail.com>
2020-09-16 09:10:31 -0300

    Commit inicial

---
+++ src/main/java/br/gov/sp/fatec/sprintbootapp/SprintBootAppApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.sprintbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/sprintbootapp/SprintBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.sprintbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
