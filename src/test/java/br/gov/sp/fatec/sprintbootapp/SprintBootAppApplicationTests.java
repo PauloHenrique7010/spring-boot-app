dadac480a9022521a6e3539da28208a0d42dd158
PauloHenrique7010 <paulohenrique7010@gmail.com>
2020-09-17 02:25:26 +0000

    Cadastrando usuario no banco

--- src/test/java/br/gov/sp/fatec/sprintbootapp/SprintBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/sprintbootapp/SprintBootAppApplicationTests.java
@@ -1,13 +1,36 @@
 package br.gov.sp.fatec.sprintbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.gov.sp.fatec.sprintbootapp.entity.Usuario;
+import br.gov.sp.fatec.sprintbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SprintBootAppApplicationTests {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
-	}
+    }
+    
+    @Test
+    void testaInsercao(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Segundo usuário");
+        usuario.setSenha("senha");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId()); 
+
+    }
 
 }
